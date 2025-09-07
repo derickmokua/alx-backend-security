d24dc1656b3f3da2c18e723a85574bc15f1d987e
derickmokua <126662232+derickmokua@users.noreply.github.com>
2025-09-07 03:52:51 +0300

    models.py

---
+++ ip_tracking/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    path = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} at {self.timestamp}"
+
+
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return f"Blocked: {self.ip_address}"
