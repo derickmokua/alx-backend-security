20ce92af7f1dd80e6914f12a5f38ec03c0894d0f
derickmokua <126662232+derickmokua@users.noreply.github.com>
2025-09-07 04:00:10 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -6,10 +6,12 @@ class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(default=timezone.now)
     path = models.CharField(max_length=255)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.ip_address} - {self.path} at {self.timestamp}"
-
+    
 
 class BlockedIP(models.Model):
     ip_address = models.GenericIPAddressField(unique=True)
