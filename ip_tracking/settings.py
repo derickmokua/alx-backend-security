911a079b5cc9567bfea774de8766d37537278c10
derickmokua <126662232+derickmokua@users.noreply.github.com>
2025-09-07 04:09:43 +0300

    Update settings.py

--- ip_tracking/settings.py
+++ ip_tracking/settings.py
@@ -1,4 +1,33 @@
-MIDDLEWARE = [
-    # Django default middleware...
-    "ip_tracking.middleware.IPLoggingMiddleware",  # add this
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # your app
+    "ip_tracking",
+    # new package
+    "ratelimit",
 ]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    # your custom middleware
+    "ip_tracking.middleware.IPLoggingMiddleware",
+]
+
+# Rate limiting configs
+RATELIMIT_ENABLE = True
+
+RATELIMIT_KEYS = {
+    "user_or_ip": "ip_tracking.views.user_or_ip",
+}
